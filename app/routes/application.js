80bc704efc66c27a76496f39745de7b386646c79
AlexKVal <alexkval@gmail.com>
2016-01-12 20:20:43 +0200

    [added] Buttons 'ON' and 'OFF'

---
+++ app/routes/application.js
@@ -0,0 +1,17 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  setupController(controller) {
+    controller.set('isOn', false);
+  },
+
+  actions: {
+    turnOn() {
+      this.controllerFor('application').set('isOn', true);
+    },
+
+    turnOff() {
+      this.controllerFor('application').set('isOn', false);
+    }
+  }
+});
