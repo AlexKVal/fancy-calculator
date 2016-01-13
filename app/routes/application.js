f56f276bfb7f16213e5e71a5fd35b7cde8f83eec
AlexKVal <alexkval@gmail.com>
2016-01-13 11:32:31 +0200

    [added] 'LEVEL' button

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,17 +1,33 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  levels: [1, 2, 3, 4],
+
   setupController(controller) {
     controller.set('isOn', false);
   },
 
   actions: {
     turnOn() {
-      this.controllerFor('application').set('isOn', true);
+      const ctrl = this.controllerFor('application');
+      ctrl.set('isOn', true);
+      ctrl.set('level', this.get('levels').objectAt(0));
     },
 
     turnOff() {
       this.controllerFor('application').set('isOn', false);
+    },
+
+    nextLevel() {
+      const ctrl = this.controllerFor('application');
+      if (ctrl.get('isOn')) {
+        const level = ctrl.get('level');
+        const levelIndex = this.get('levels').indexOf(level);
+        const nextLevel = this.get('levels').objectAt(
+          (levelIndex + 1) % this.get('levels').length
+        );
+        ctrl.set('level', nextLevel);
+      }
     }
   }
 });
