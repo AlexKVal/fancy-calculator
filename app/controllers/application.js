eec55540440c9aa22baea1563aeff30d23585851
AlexKVal <alexkval@gmail.com>
2016-01-15 17:57:53 +0200

    [added] 'mode' funcionality and actions moved to the controller

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,13 +1,58 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  isOn: false,
+  levels: [1, 2, 3, 4],
+
+  isOn: null,
   level: null,
+  mode: null,
+
+  inSettingsMode: Ember.computed('isOn', 'mode', function() {
+    return this.get('isOn') && this.get('mode') === 'settings';
+  }),
+
+  inGameMode: Ember.computed('isOn', 'mode', function() {
+    return this.get('isOn') && this.get('mode') === 'game';
+  }),
 
   actions: {
+    turnOn() {
+      this.set('isOn', true);
+      this.set('level', this.get('levels').objectAt(0));
+      this.set('operator', '+');
+      this.set('mode', 'settings');
+    },
+
+    turnOff() {
+      this.set('isOn', false);
+    },
+
     changeOperator(operator) {
-      if (this.get('isOn')) {
+      if (this.get('inSettingsMode')) {
         this.set('operator', operator);
+      }
+    },
+
+    playGame() {
+      if (this.get('inSettingsMode')) {
+        this.set('mode', 'game');
+      }
+    },
+
+    changeSettings() {
+      if (this.get('inGameMode')) {
+        this.set('mode', 'settings');
+      }
+    },
+
+    nextLevel() {
+      if (this.get('inSettingsMode')) {
+        const level = this.get('level');
+        const levelIndex = this.get('levels').indexOf(level);
+        const nextLevel = this.get('levels').objectAt(
+          (levelIndex + 1) % this.get('levels').length
+        );
+        this.set('level', nextLevel);
       }
     }
   }
--- app/routes/application.js
+++ app/routes/application.js
@@ -1,34 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  levels: [1, 2, 3, 4],
-
-  setupController(controller) {
-    controller.set('isOn', false);
-  },
-
-  actions: {
-    turnOn() {
-      const ctrl = this.controllerFor('application');
-      ctrl.set('isOn', true);
-      ctrl.set('level', this.get('levels').objectAt(0));
-      ctrl.set('operator', '+');
-    },
-
-    turnOff() {
-      this.controllerFor('application').set('isOn', false);
-    },
-
-    nextLevel() {
-      const ctrl = this.controllerFor('application');
-      if (ctrl.get('isOn')) {
-        const level = ctrl.get('level');
-        const levelIndex = this.get('levels').indexOf(level);
-        const nextLevel = this.get('levels').objectAt(
-          (levelIndex + 1) % this.get('levels').length
-        );
-        ctrl.set('level', nextLevel);
-      }
-    }
-  }
 });
