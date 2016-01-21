4c44d936e899fbce7dbcc8aa62b5861b1332982a
AlexKVal <alexkval@gmail.com>
2016-01-21 20:10:56 +0200

    [added] use keyboard for convenience

--- app/components/labeled-button.js
+++ app/components/labeled-button.js
@@ -1,6 +1,18 @@
 import Ember from 'ember';
+import { EKMixin, keyDown, getKey } from 'ember-keyboard';
 
-export default Ember.Component.extend({
+export default Ember.Component.extend(EKMixin, {
+  activateKeyboard: Ember.on('init', function() {
+    this.set('keyboardActivated', true);
+  }),
+  onKeyDown: Ember.on(keyDown(), function(event) {
+    const keyPressed = getKey(event);
+    const buttonValue = this.get('value');
+    if (keyPressed == buttonValue) {
+      this.get('onPress')(buttonValue);
+    }
+  }),
+
   actions: {
     press() {
       this.get('onPress')(this.get('value'));
